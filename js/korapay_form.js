2b784aabf7c6d233a902c3f13d660ddcd5e89706
Divine Olokor <divee789@gmail.com>
2020-05-02 21:33:19 +0100

    updated checkout modal parameters

--- js/korapay_form.js
+++ js/korapay_form.js
@@ -14,25 +14,19 @@
                     reference: options.reference + '_' + new Date().getTime(),
                     amount: Number(options.amount),
                     currency: 'NGN',
-                    channels: ['card', 'bank_transfer'],
-                    narration: 'Paying for order',
-                    customerName: `${options.first_name} ${options.last_name}`,
-                    customerEmail: options.email,
+                    customer:{
+                        name: `${options.first_name} ${options.last_name}`,
+                        email: options.email
+                    },
                     onClose: function () {
-                        console.log(':weary:, you are gone')
-
                         // Simulate an HTTP redirect to the payment cancelled url
                         window.location.replace(options.cancel_action)
                     },
                     onSuccess: function (data) {
-                        console.log(data)
-
                         // Simulate an HTTP redirect to the payment return url
                         window.location.replace(options.redirect_url + `?resp=success&reference=${data.reference}`);
                     },
                     onFailed: function (data) {
-                        console.log(data)
-
                         // Simulate an HTTP redirect to
                         window.location.replace(options.redirect_url + `?resp=failed&reference=${data.reference}`)
                     }
